752c54b4fc4aa2e769fb5feb7e2fb601f3491f28
YashasviKote <yashasvikote05@gmail.com>
2026-02-14 19:03:37 +0530

    Add files via upload

---
+++ PatternGenerator.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class PatternGenerator {
+
+    static void square(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void triangle(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void numberTriangle(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Enter rows: ");
+            int n = sc.nextInt();
+
+            System.out.println("\nSquare Pattern:");
+            square(n);
+
+            System.out.println("\nStar Triangle:");
+            triangle(n);
+
+            System.out.println("\nNumber Triangle:");
+            numberTriangle(n);
+        }
+    }
+}
